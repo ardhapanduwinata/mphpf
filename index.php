cd7a31289bfba17391179bf82f2fa87842926c77
ardhapanduwinata <33822238+ardhapanduwinata@users.noreply.github.com>
2021-03-13 09:27:07 +0700

    zzz

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $settings = Settings::getInstance();
 
 // The app folder relative to your htdocs folder.
 // $settings->setAppFolder('/MPHPFramework');
-$settings->setAppFolder('/ardhapanduwinata/mphpf');
+$settings->setAppFolder('/');
 
 // Add your DB settings. This is not the best practice, but it is okay for now.
 $settings->setDbConnection(array(
