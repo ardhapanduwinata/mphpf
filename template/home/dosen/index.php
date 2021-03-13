d645c7bb089f9204f97894bae80ee9812584e81a
ardhapanduwinata <ardhapanduwinata@gmail.com>
2021-03-13 08:12:52 +0700

    menambahkan fitur data dosen, add data dosen & view data dosen

---
+++ template/home/dosen/index.php
@@ -0,0 +1,24 @@
+<h2>Data Dosen</h2>
+<a href="<?php echo $this->homeAddress('/add-dosen'); ?>">Tambah Dosen Baru</a>
+<br/>
+<br/>
+<table class="data-table">
+    <thead class="data-table-th">
+        <td class="data-table-td">NIP</td>
+        <td class="data-table-td">Nama</td>
+        <td class="data-table-td">Alamat</td>
+        <td class="data-table-td">Prodi Jurusan</td>
+        <td class="data-table-td">No. Telepon</td>
+    </thead>
+    <tbody>
+    <?php foreach ($this->data('all_dosen') as $dosen) { ?>
+        <tr class="data-table-tr">
+            <td class="data-table-td"><?= $dosen['nip'] ?></td>
+            <td class="data-table-td"><?= $dosen['nama'] ?></td>
+            <td class="data-table-td"><?= $dosen['alamat'] ?></td>
+            <td class="data-table-td"><?= $dosen['prodiJurusan']?></td>
+            <td class="data-table-td"><?= $dosen['notelp'] ?></td>
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
