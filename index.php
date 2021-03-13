b2dd85f35be01583f3bfd873f46d929e2431807c
ardhapanduwinata <33822238+ardhapanduwinata@users.noreply.github.com>
2021-03-13 08:48:01 +0700

    gatau lg dah

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $settings = Settings::getInstance();
 
 // The app folder relative to your htdocs folder.
 // $settings->setAppFolder('/MPHPFramework');
-$settings->setAppFolder('/https://github.com/ardhapanduwinata/mphpf');
+$settings->setAppFolder('/mphpf.herokuapp.com');
 
 // Add your DB settings. This is not the best practice, but it is okay for now.
 $settings->setDbConnection(array(
